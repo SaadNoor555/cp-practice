c9f4d79ac0c19e8373b54c132b6b1dfb985bde72
SaadNoor555 <48786753+SaadNoor555@users.noreply.github.com>
2020-12-16 16:27:39 +0600

    Windows DP

--- DpCoinChange.cpp
+++ DpCoinChange.cpp
@@ -1,100 +1,170 @@
-#include<bits/stdc++.h>
 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<stdio.h>
+#include<math.h>
+#include<queue>
+#include<set>
+#include<numeric>
+#include<bitset>
+#include<limits>
+
+#define ELIF else if
+#define FOR(iterator, limit, adder) for(int iterator=0; iterator<limit; iterator+=adder)
+#define PI 3.1415926535897932384626433832795
+#define PB push_back
+#define OutNO cout<<"NO\n"
+#define OutYES cout<<"YES\n"
+#define Out(n) cout<< n<<"\n"
+#define vint vector<int>
+#define vll vector<ll>
+#define vpiin vector<pair<int, int>>
+#define piin pair<int, int>
+#define qint queue<int>
+#define RED 1
+#define BLACK 2
+
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
 typedef long long ll;
 
 using namespace std;
 
-template<typename T>
-//order matters
-//coins= coin array, n=size of array, val=required value
-ll InfCoinSwapOrder(vector<T>coins, T n, T val)
+void InArll(ll *ar, int n)
 {
-    ll dp[val+5][n+5];
-    memset(dp, 0, sizeof dp);
+  FOR(i, n, 1) cin>> ar[i];
+}
 
-    for(int i=0; i<n; i++)
-        dp[0][i]= 1;
+void OutArll(ll *a, int n)
+{
+  FOR(i, n, 1) cout<< a[i]<<" ";
+  cout<<"\n";
+}
 
-    for(int sum=1; sum<=val; sum++)
-        for(int idx=0; idx<n; idx++)
+void InAr(int *ar, int n)
+{
+  FOR(i, n, 1) cin>> ar[i];
+}
+
+void OutAr(int *a, int n)
+{
+  FOR(i, n, 1) cout<< a[i]<<" ";
+  cout<<"\n";
+}
+
+void OutVin(vint a)
+{
+    FOR(i, a.size(), 1)
+        cout<< a[i]<< " ";
+
+    cout<< "\n";
+}
+
+void OutVll(vll a)
+{
+    FOR(i, a.size(), 1)
+        cout<< a[i]<< " ";
+
+    cout<< "\n";
+}
+
+vint Dijkstra(int source, int n, vector<vpiin> graph)
+{
+    vint cost(n, 99999);
+	cost[source]= 0;
+	vector<vint> ans(n);
+    int active;
+    priority_queue<piin, vpiin,greater<piin>> pq;
+    pq.push({0, source});
+	bitset<100001> visited;
+	// cout<<pq.size()<<"\n";
+	// FOR(i, n, 1) cout<< graph[i].size()<< " ";
+	// cout<< "\n";
+    while(!pq.empty())
+    {
+        active= pq.top().second;
+        pq.pop();
+		if(visited[active]) continue;
+		visited[active]= 1;
+		// cout<< active<< " "<< cost[active]<<"\n";
+        FOR(i, graph[active].size(), 1)
         {
-            //x= if we take coin[idx], y= if we skip coin[idx]
-            ll x= sum-coins[idx]>=0? dp[sum-coins[idx]][idx] : 0;
-            ll y= idx? dp[sum][idx-1] : 0;
-            dp[sum][idx]= x+y;
+			piin buf= graph[active][i];
+            if(cost[buf.second]>buf.first+cost[active])
+            {
+                cost[buf.second]= buf.first+cost[active];
+				ans[buf.second]= ans[active];
+				ans[buf.second].PB(active);
+                pq.push({cost[buf.second], buf.second});
+            }
         }
-        
-    return dp[val][n-1];
+		// cout<<"hi\n";
+    }
+	return cost;
 }
 
-template<typename T>
-//order does not matter
-ll InfCoinSwapUnorder(vector<T>coins, T n, T val)
+ll count(ll *a, int n, ll v)
 {
-    ll dp[val+5];
-    memset(dp, 0, sizeof dp);
-    dp[0]= 1;
-    for(int i=0; i<=val; i++)
-        for(int idx=0; idx<n; idx++)
-            if(i+coins[idx]<=val)
-                dp[i+coins[idx]]+=dp[i];
+	ll c=0;
+	FOR(i, n, 1)
+		if(a[i]>=v) c++;
 
-    return dp[val];
+	return c;
 }
 
-template<typename T>
-//order matters
-ll SingleCoinSwapOrder(vector<T> coins, T n, T val)
+void MEMSET(ll *a, int n, int v)
 {
-    ll dp[val+5];
-    memset(dp, 0, sizeof dp);
-    dp[0]= 1;
-
-    /*we iterate over coins array first, and value from behind.
-     so no elemnt is repeated*/
-    for(int idx=0; idx<n; idx++)
-        for(int sm=val; sm>=0; sm--)
-        {
-            if(sm-coins[idx]>=0) dp[sm]+=dp[sm-coins[idx]];
-            else break;
-        }
-
-    return dp[val];
+	FOR(i, n, 1)
+		a[i]= v;
 }
 
-template<typename T>
-//change the value of MAX coins required before use
-ll MinCoinChange(vector<T>coins, T n, T val)
+int gcd(int a, int b) 
 {
-    MAX= 10001;
-    ll dp[val+5];
-    for(int i=1; i<val+5; i++)
-        dp[i]= MAX;
-    dp[0]= 0;
-    sort(coins.begin(), coins.end());
-    
-    for(int sm=1; sm<=val; sm++)
-        for(int idx=0; idx<n; idx++)
-            if(sm-coins[idx]>=0) dp[sm]= min(dp[sm], dp[sm-coins[idx]]+1);
-
-    if(dp[val]==MAX) dp[val]= -1;
-    return dp[val];
+   if (b == 0)
+   return a;
+   return gcd(b, a % b);
 }
-
 
 int main()
 {
-    int n, m;
-    vector<int> cn;
-    cin>> n>> m;
-    int t1;
-    for(int i=0; i<m; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>> t;
+    // t=1;
+    while(t--)
     {
-        cin>> t1;
-        cn.push_back(t1);
-    }
+        int n, s;
+        cin>> n>> s;
+        int a[n];
+        InAr(a, n);
 
-    // cout<< InfCoinSwapUnorder<int>(cn, m, n)<< '\n';
-    // cout<< InfCoinSwapOrder<int>(cn, m, n)<< '\n';
-    // cout<< MinCoinChange<int>(cn, m, n)<< '\n';
+        int mem[n+1][s+1];
+
+        FOR(i, n+1, 1)
+            FOR(j, s+1, 1)
+                mem[i][j]= 0;
+
+        FOR(i, n+1, 1)
+            mem[i][0]= 1;
+
+        for(int idx=n-1; idx>=0; idx--)
+            for(int sum=1; sum<=s; sum++)
+                mem[idx][sum]=((sum-a[idx]>=0)? mem[idx][sum-a[idx]] : 0)+((idx<n-1)? mem[idx+1][sum] : 0);
+
+        // FOR(i, n+1, 1)
+        // {
+        //     FOR(j, s+1, 1)
+        //         cout<< mem[i][j]<< " ";
+        //     cout<< "\n";
+        // }
+
+        Out(mem[0][s]);
+    }
 }
